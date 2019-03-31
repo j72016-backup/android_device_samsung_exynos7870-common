2555a072e342090ce30e4fef58189f1b526d40de
your name <your_email@your_domain.com>
2019-03-31 17:52:32 +0000

    test

--- board/board.mk
+++ board/board.mk
@@ -26,10 +26,10 @@ TARGET_BOOTLOADER_BOARD_NAME := universal7870
 
 # CPU
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_CORTEX_A53 := true
 
 # Binder
