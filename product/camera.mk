820580ed3342da022dcd4039466dbc5349ce5e23
your name <your_email@your_domain.com>
2019-04-05 14:56:15 +0000

    Kang S7 Camera config @IvanMeler

--- product/camera.mk
+++ product/camera.mk
@@ -1,20 +1,4 @@
-# Copyright (C) 2018 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-#Camera
- PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     camera.universal7870 \
     libexynoscamera_shim \
     libstagefright_shim \
@@ -22,7 +6,8 @@
     camera.device@3.2-impl \
     camera.device@1.0-impl \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    Camera2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/external_camera_config.xml:system/vendor/etc/external_camera_config.xml
