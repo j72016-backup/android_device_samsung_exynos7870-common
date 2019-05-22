9ea2ef92998e69d8ad2008515b3b9ba1356ff8eb
yEsPaP <46491239+yEsPaP@users.noreply.github.com>
2019-05-22 06:59:12 +0300

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -2,3 +2,8 @@
 # Define default initial receive window size in segments.
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tcp.default_init_rwnd=60
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.adb.secure=0 \
+    sys.usb.ffs.aio_compat=1
