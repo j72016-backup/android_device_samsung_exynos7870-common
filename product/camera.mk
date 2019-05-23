cd36cc92747271b864665016752d87563efa8f62
yEsPaP <h.sachmanovas@gmail.com>
2019-05-23 15:01:54 +0000

    N

--- product/camera.mk
+++ product/camera.mk
@@ -3,7 +3,9 @@ PRODUCT_PACKAGES += \
     libexynoscamera_shim \
     libstagefright_shim \
     libcamera_client_shim \
-    android.hardware.camera.provider@2.4-impl.exynos7870 \
+    camera.device@3.2-impl \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
 PRODUCT_COPY_FILES += \
